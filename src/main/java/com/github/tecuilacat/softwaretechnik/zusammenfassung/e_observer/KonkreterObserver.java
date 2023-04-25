d778923f52500ce9febd7138f98d97feae9845ed
TecuilaCat <oliver-steck@gmx.de>
2023-04-25 15:25:50 +0200

    finalized Observer Pattern

--- src/main/java/com/github/tecuilacat/softwaretechnik/zusammenfassung/e_observer/KonkreterObserver.java
+++ src/main/java/com/github/tecuilacat/softwaretechnik/zusammenfassung/e_observer/KonkreterObserver.java
@@ -2,10 +2,16 @@ package com.github.tecuilacat.softwaretechnik.zusammenfassung.e_observer;
 
 public class KonkreterObserver implements Observer {
 
+    private String name;
+
+    public KonkreterObserver(String name) {
+        this.name = name;
+    }
+
     @Override
     public void aktualisiere(Subjekt subjekt) {
         if (subjekt instanceof KonkretesSubjekt subj) {
-            System.out.println("Im Subjekt " + subj.getName() +  " hat sich der Zustand zu "+ subj.getZustand() + " geändert!");
+            System.out.println(name + ": Im Subjekt " + subj.getName() +  " hat sich der Zustand zu "+ subj.getZustand() + " geändert!");
         }
     }
 
--- src/main/java/com/github/tecuilacat/softwaretechnik/zusammenfassung/e_observer/ObserverTest.java
+++ src/main/java/com/github/tecuilacat/softwaretechnik/zusammenfassung/e_observer/ObserverTest.java
@@ -2,16 +2,35 @@ package com.github.tecuilacat.softwaretechnik.zusammenfassung.e_observer;
 
 public class ObserverTest {
 
-    public static void main(String[] args) {
-        KonkretesSubjekt subjA = new KonkretesSubjekt("A");
-        KonkretesSubjekt subjB = new KonkretesSubjekt("B");
+    public static void main(String[] args) throws Exception {
+        //Mehrere Subjekte zu einem Observer
+        KonkretesSubjekt subjA = new KonkretesSubjekt("subjA");
+        KonkretesSubjekt subjB = new KonkretesSubjekt("subjB");
 
-        Observer obs1 = new KonkreterObserver();
+        Observer obs1 = new KonkreterObserver("obs1");
         subjA.subscribe(obs1);
         subjB.subscribe(obs1);
 
         subjA.setZustand(5);
         subjB.setZustand(10);
+
+
+        Thread.sleep(4000);
+        //Alternative: Mehrere Observer zu einem Subjekt
+        KonkreterObserver konkreterObserver1 = new KonkreterObserver("konkreterObserver1");
+        KonkreterObserver konkreterObserver2 = new KonkreterObserver("konkreterObserver2");
+
+        KonkretesSubjekt subjekt = new KonkretesSubjekt("subjekt");
+        subjekt.subscribe(konkreterObserver1);
+        subjekt.subscribe(konkreterObserver2);
+
+        subjekt.setZustand(100);
+        Thread.sleep(2000);
+        subjekt.setZustand(200);
+        Thread.sleep(2000);
+        subjekt.setZustand(300);
+        Thread.sleep(2000);
+        subjekt.setZustand(400);
     }
 
 }
